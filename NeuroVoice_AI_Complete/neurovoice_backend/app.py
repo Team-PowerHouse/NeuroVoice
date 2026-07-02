1200a02b28196b997b28a190562d7ac6cd5ca235
Harezz18 <haresh03072008@gmail.com>
2026-07-02 14:23:38 +0530

    Update app.py

--- NeuroVoice_AI_Complete/neurovoice_backend/app.py
+++ NeuroVoice_AI_Complete/neurovoice_backend/app.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://neuro-voice-three.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
